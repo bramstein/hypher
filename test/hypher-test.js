a14a4490cae8e2dc2c598e72f475a452f6c7b94b
Bram Stein <b.l.stein@gmail.com>
2011-03-01 23:25:34 +0100

    Added tests for Trie creation. Added support for compound word hyphenation. Hypher now also skips words that are already hyphenated.

--- test/hypher-test.js
+++ test/hypher-test.js
@@ -38,10 +38,57 @@ words.forEach(function (word) {
 vows.describe('Hypher').addBatch({
     'hyphenate ': {
         topic: function () {
-            return new hypher.Hypher(language, {
-                minLength: 4
-            });
+            return new hypher.Hypher(language);
         },
         'word: ': dictionary
+    },
+    'trie': {
+        topic: function () {
+            return new hypher.Hypher({
+                patterns: {
+                    "2": "a1b2",
+                    "3": "a2bb3c"
+                }
+            });
+        },
+        'trie is correctly generated': function (h) {
+            assert.deepEqual(h.trie, {
+                "a": {
+                    _points: [0, 1],
+                    "b": {
+                        _points: [0, 2, 0]
+                    }
+                },
+                "b": {
+                    _points: [0, 2],
+                    "c": {
+                        _points: [0, 3, 0]
+                    }
+                }
+            });
+        }
+    },
+    'hyphenate with soft hyphens': {
+        topic: function () {
+            return new hypher.Hypher(language);
+        },
+        'hyph\u00ADen': hyphenatesTo(['hyph\u00ADen'])
+    },
+    'hyphenate with en-dash, hyphen-minus or hyphen': {
+        topic: function () {
+            return new hypher.Hypher(language);
+        },
+        // The resulting hyphenation might look odd, but a minus-hyphen, en-dash, or hyphen will
+        // be broken by the browser, so we don't need to insert a soft hyphen in that position.
+        'bootstrapping-brainstorm': hyphenatesTo(['boot', 'strap', 'ping-brain', 'storm']),
+
+        // hyphen-minus
+        'bootstrapping\u002Dbrainstorm': hyphenatesTo(['boot', 'strap', 'ping\u002Dbrain', 'storm']),
+
+        // hyphen
+        'bootstrapping\u2010brainstorm': hyphenatesTo(['boot', 'strap', 'ping\u2010brain', 'storm']),
+
+        // en-dash
+        'bootstrapping\u2013brainstorm': hyphenatesTo(['boot', 'strap', 'ping\u2013brain', 'storm']),
     }
 }).export(module);
